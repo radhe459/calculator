9a719366e79e67ee480f68619a1acfa4da295172
RADHE SHYAM CHAUBEY <informativersc1234@gmail.com>
2024-08-16 10:24:09 +0530

    handled negative numbers, handled calculation when nothing in display handled decimal numbers, Added single digit removing feature.

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ function appendToDisplay(item) {
     item = item.toString()
     let currValue = display.value
     let currValueArray = currValue.toString().split("");
-    if (isNaN(Number(currValueArray[currValueArray.length - 1])) && isNaN(Number(item))) {
-        currValueArray[currValueArray.length - 1] = item;
-    }
-    else {
-        currValueArray.push(item);
-    }
+    constructDisplayArray(currValueArray, item)
     console.log(currValueArray)
     display.value = currValueArray.join("")
 }
@@ -18,19 +13,47 @@ function clearDisplay() {
     display.value = "";
 }
 
+function remove() {
+    let currValueArray = display.value.toString().split("")
+    currValueArray.pop()
+    display.value = currValueArray.join("");
+}
+
 function calculate() {
     let currValuesArray = display.value.toString().split("");
     let result = performCalculation(currValuesArray)
     display.value = result
 }
 
+function constructDisplayArray(currValueArray, item) {
+    if (currValueArray.length === 0 && item === "-") {
+        currValueArray.push("-")
+    }
+
+    else if (isNaN(Number(currValueArray[currValueArray.length - 1])) && isNaN(Number(item))) {
+        display.value = display.value == "" ? "0" : display.value;
+        handleNegativeAndDecimalCases(currValueArray, item);
+    }
+    else {
+        currValueArray.push(item);
+    }
+}
+
+
+function handleNegativeAndDecimalCases(currValueArray, item) {
+    let shouldChange = !(currValueArray.length == 1 && currValueArray[0] === "-") && !(currValueArray[currValueArray.length - 1] === ".");
+    if (shouldChange) {
+        currValueArray[currValueArray.length - 1] = item;
+    }
+}
+
 
 function performCalculation(currValuesArray) {
-    let result = "0"
+    let result = ""
     let operator = null
-    let curValue = "0"
+    let curValue = ""
     currValuesArray.forEach(element => {
-        if (isNaN(element)) {
+        if (isNaN(element) && element !== ".") {
             result = performOperation(result, curValue, operator).toString()
             operator = element
             curValue = "0"
